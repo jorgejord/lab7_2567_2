814c25ad66dd2e046148cb5e587c261769133668
jorgejord <supasatchai@gmail.com>
2024-12-04 18:18:14 +0700

    https://github.com/jorgejord/lab7_2567_2.git

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,9 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char input) {
+    if (input >= 'A' && input <= 'Z') {
+        return input == 'A' ? 'Z' : input - 1;
+    }
+    return '0';
 }
